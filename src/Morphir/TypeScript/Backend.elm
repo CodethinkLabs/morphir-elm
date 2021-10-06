e7dbbe617b756193ebaeeb16bbb4d5e59fafe70a
Douglas Winship <douglas.winship@codethink.co.uk>
2021-10-06 01:41:58 +0100

    Review Feedback: reorganize functions (Part 2: mapTypes)

--- src/Morphir/TypeScript/Backend.elm
+++ src/Morphir/TypeScript/Backend.elm
@@ -1,7 +1,6 @@
 module Morphir.TypeScript.Backend exposing
     ( Options
     , mapDistribution
-    , mapTypeDefinition
     )
 
 {-| This module contains the TypeScript backend that translates the Morphir IR into TypeScript.
@@ -20,6 +19,7 @@ import Morphir.IR.Package as Package
 import Morphir.IR.Path as Path exposing (Path)
 import Morphir.IR.Type as Type exposing (Type)
 import Morphir.TypeScript.AST as TS
+import Morphir.TypeScript.Backend.MapTypes exposing (mapPrivacy, mapTypeDefinition)
 import Morphir.TypeScript.NamespaceMerger exposing (mergeNamespaces)
 import Morphir.TypeScript.PrettyPrinter as PrettyPrinter exposing (getTypeScriptPackagePathAndModuleName)
 
@@ -168,7 +168,7 @@ mapModuleDefinition opt distribution currentPackagePath currentModulePath access
                 , content = typeDefs
                 }
 
-        {--Collect refernces from inside the module,
+        {--Collect references from inside the module,
         filter out references to current module
         then sort references and get a list of unique references-}
         moduleUnit : TS.CompilationUnit
@@ -257,155 +257,3 @@ collectRefsFromTypeExpression typeExp =
 
         _ ->
             []
-
-
-{-| Map a Morphir Constructor (A tuple of Name and Constructor Args) to a Typescript AST Interface
--}
-mapConstructor : TS.Privacy -> List TS.TypeExp -> ( Name, List ( Name, Type.Type ta ) ) -> TS.TypeDef
-mapConstructor privacy variables ( ctorName, ctorArgs ) =
-    let
-        kindField =
-            ( "kind", TS.LiteralString (ctorName |> Name.toTitleCase) )
-
-        otherFields =
-            ctorArgs
-                |> List.map
-                    (\( argName, argType ) ->
-                        ( argName |> Name.toCamelCase, mapTypeExp argType )
-                    )
-    in
-    TS.Interface
-        { name = ctorName
-        , privacy = privacy
-        , variables = variables
-        , fields = kindField :: otherFields
-        }
-
-
-{-| Map a Morphir type definition into a list of TypeScript type definitions. The reason for returning a list is that
-some Morphir type definitions can only be represented by a combination of multiple type definitions in TypeScript.
--}
-mapTypeDefinition : Name -> AccessControlled (Documented (Type.Definition ta)) -> List TS.TypeDef
-mapTypeDefinition name typeDef =
-    let
-        doc =
-            typeDef.value.doc
-
-        privacy =
-            typeDef.access |> mapPrivacy
-    in
-    case typeDef.value.value of
-        Type.TypeAliasDefinition variables typeExp ->
-            [ TS.TypeAlias
-                { name = name
-                , privacy = privacy
-                , doc = doc
-                , variables = variables |> List.map Name.toCamelCase |> List.map (\var -> TS.Variable var)
-                , typeExpression = typeExp |> mapTypeExp
-                }
-            ]
-
-        Type.CustomTypeDefinition variables accessControlledConstructors ->
-            let
-                tsVariables =
-                    variables |> List.map Name.toCamelCase |> List.map (\var -> TS.Variable var)
-
-                constructors =
-                    accessControlledConstructors.value
-                        |> Dict.toList
-
-                constructorNames =
-                    accessControlledConstructors.value
-                        |> Dict.keys
-
-                constructorInterfaces =
-                    constructors
-                        |> List.map (mapConstructor privacy tsVariables)
-
-                union =
-                    if List.all ((==) name) constructorNames then
-                        []
-
-                    else
-                        List.singleton
-                            (TS.TypeAlias
-                                { name = name
-                                , privacy = privacy
-                                , doc = doc
-                                , variables = tsVariables
-                                , typeExpression =
-                                    TS.Union
-                                        (constructors
-                                            |> List.map
-                                                (\( ctorName, _ ) ->
-                                                    TS.TypeRef (FQName.fQName [] [] ctorName) tsVariables
-                                                )
-                                        )
-                                }
-                            )
-            in
-            union ++ constructorInterfaces
-
-
-{-| Map a Morphir type expression into a TypeScript type expression.
--}
-mapTypeExp : Type.Type ta -> TS.TypeExp
-mapTypeExp tpe =
-    case tpe of
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "basics" ] ], [ "bool" ] ) [] ->
-            TS.Boolean
-
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "dict" ] ], [ "dict" ] ) [ dictKeyType, dictValType ] ->
-            TS.List (TS.Tuple [ mapTypeExp dictKeyType, mapTypeExp dictValType ])
-
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "list" ] ], [ "list" ] ) [ listType ] ->
-            TS.List (mapTypeExp listType)
-
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "basics" ] ], [ "float" ] ) [] ->
-            TS.Number
-
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "basics" ] ], [ "int" ] ) [] ->
-            TS.Number
-
-        Type.Record _ fieldList ->
-            TS.Object
-                (fieldList
-                    |> List.map
-                        (\field ->
-                            ( field.name |> Name.toCamelCase, mapTypeExp field.tpe )
-                        )
-                )
-
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "char" ] ], [ "char" ] ) [] ->
-            TS.String
-
-        Type.Reference _ ( [ [ "morphir" ], [ "s", "d", "k" ] ], [ [ "string" ] ], [ "string" ] ) [] ->
-            TS.String
-
-        Type.Tuple _ tupleTypesList ->
-            TS.Tuple (List.map mapTypeExp tupleTypesList)
-
-        Type.Reference _ fQName typeList ->
-            TS.TypeRef fQName (typeList |> List.map mapTypeExp)
-
-        Type.Unit _ ->
-            TS.Tuple []
-
-        Type.Variable _ name ->
-            TS.Variable (Name.toCamelCase name)
-
-        Type.ExtensibleRecord a argName fields ->
-            TS.UnhandledType "ExtensibleRecord"
-
-        Type.Function a argType returnType ->
-            TS.UnhandledType "Function"
-
-
-mapPrivacy : Access -> TS.Privacy
-mapPrivacy privacy =
-    case privacy of
-        Public ->
-            TS.Public
-
-        Private ->
-            TS.Private
