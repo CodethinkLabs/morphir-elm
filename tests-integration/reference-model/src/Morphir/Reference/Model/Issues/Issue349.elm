29c093df26b8eee574d67992ffa1ede95810aad6
Douglas Winship <douglas.winship@codethink.co.uk>
2021-10-14 11:07:33 +0100

    REMOVE THIS COMMIT: add some details to issue 349 in reference model

--- tests-integration/reference-model/src/Morphir/Reference/Model/Issues/Issue349.elm
+++ tests-integration/reference-model/src/Morphir/Reference/Model/Issues/Issue349.elm
@@ -1,13 +1,34 @@
 module Morphir.Reference.Model.Issues.Issue349 exposing (..)
 
 
-type alias Result a =
-    { foo : List a
+type MyCustom dog cat pigeon
+    = Foo Int dog
+    | Bar Int cat
+    | Baz Int pigeon
+
+
+type MyOtherCustom dog cat pigeon
+    = OtherFoo Int dog
+    | OtherBar (List cat)
+    | OtherBaz { somefield : dog, otherfield : pigeon }
+
+
+type alias Result a b c =
+    { foo : a
+    , bar : List b
+    , baz : c
+    , qux : Int
     }
 
 
-f : Result a
+type alias StringResultIntBool =
+    Result String Int Bool
+
+
+f : Result Bool a Bool
 f =
-    { foo =
-        []
+    { foo = True
+    , bar = []
+    , baz = False
+    , qux = 2
     }
