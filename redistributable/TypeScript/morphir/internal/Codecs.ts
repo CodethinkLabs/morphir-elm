38f27e1df00fdfbdb1803e543e0838e104f0386c
Sam Thursfield <sam.thursfield@codethink.co.uk>
2021-10-22 17:59:37 +0200

    Codecs.ts: Format with Prettier
    For consistency with generated code

--- redistributable/TypeScript/morphir/internal/Codecs.ts
+++ redistributable/TypeScript/morphir/internal/Codecs.ts
@@ -16,8 +16,8 @@ limitations under the License.
 
 class DecodeError extends Error {}
 
-Object.defineProperty(DecodeError.prototype, 'name', {
-    value: 'DecodeError',
+Object.defineProperty(DecodeError.prototype, "name", {
+  value: "DecodeError",
 });
 
 type GenericDecoder = (input: any) => any;
@@ -30,233 +30,255 @@ type EncoderList = Array<GenericEncoder>;
 type EncoderMap = Map<string, GenericEncoder>;
 
 export function decodeUnit(input: any): [] {
-    return [];
+  return [];
 }
 
 export function decodeBoolean(input: any): boolean {
-    if (typeof(input) != "boolean") {
-        throw new DecodeError(`Expected bool, got ${typeof(input)}`);
-    }
-    return input;
+  if (typeof input != "boolean") {
+    throw new DecodeError(`Expected bool, got ${typeof input}`);
+  }
+  return input;
 }
 
 export function decodeChar(input: any): string {
-    if (typeof(input) != "string") {
-        throw new DecodeError(`Expected char, got ${typeof(input)}`);
-    }
-    if (input.length != 1) {
-        throw new DecodeError(`Expected char, got string`);
-    }
-    return input;
+  if (typeof input != "string") {
+    throw new DecodeError(`Expected char, got ${typeof input}`);
+  }
+  if (input.length != 1) {
+    throw new DecodeError(`Expected char, got string`);
+  }
+  return input;
 }
 
 export function decodeString(input: any): string {
-    if (typeof(input) != "string") {
-        throw new DecodeError(`Expected string, got ${typeof(input)}`);
-    }
-    return input;
+  if (typeof input != "string") {
+    throw new DecodeError(`Expected string, got ${typeof input}`);
+  }
+  return input;
 }
 
 export function decodeInt(input: any): number {
-    if (typeof(input) != "number") {
-        throw new DecodeError(`Expected int, got ${typeof(input)}`);
-    }
-    return input;
+  if (typeof input != "number") {
+    throw new DecodeError(`Expected int, got ${typeof input}`);
+  }
+  return input;
 }
 
 export function decodeFloat(input: any): number {
-    if (typeof(input) != "number") {
-        throw new DecodeError(`Expected float, got ${typeof(input)}`);
-    }
-    return input;
+  if (typeof input != "number") {
+    throw new DecodeError(`Expected float, got ${typeof input}`);
+  }
+  return input;
 }
 
 export function decodeCustomType(decoderMap: DecoderMap, input: any): object {
-    if (typeof input == "string") input = [input];
-    if (!(input instanceof Array)) {
-        throw new DecodeError(`Expected Array, got ${typeof input}`);
-    }
-    if (!(typeof input[0] == "string")) {
-        throw new DecodeError(`Expected String, got ${typeof input}`);
-    }
-    if (!(decoderMap.has(input[0]))) {
-        let variantNames = Array.from(decoderMap.keys());
-        let variantNameString = variantNames.join(", ")
-        throw new DecodeError(`Expected one of "${variantNameString}", got ${input[0]}`);
-    }
-    return decoderMap.get(input[0])(input)
+  if (typeof input == "string") input = [input];
+  if (!(input instanceof Array)) {
+    throw new DecodeError(`Expected Array, got ${typeof input}`);
+  }
+  if (!(typeof input[0] == "string")) {
+    throw new DecodeError(`Expected String, got ${typeof input}`);
+  }
+  if (!decoderMap.has(input[0])) {
+    let variantNames = Array.from(decoderMap.keys());
+    let variantNameString = variantNames.join(", ");
+    throw new DecodeError(
+      `Expected one of "${variantNameString}", got ${input[0]}`
+    );
+  }
+  return decoderMap.get(input[0])(input);
 }
 
-export function decodeCustomTypeVariant(kind: string,
-                                        argNames: Array<string>,
-                                        argDecoders: DecoderList,
-                                        input: any): object {
-    if (typeof input == "string") input = [input];
+export function decodeCustomTypeVariant(
+  kind: string,
+  argNames: Array<string>,
+  argDecoders: DecoderList,
+  input: any
+): object {
+  if (typeof input == "string") input = [input];
 
-    if (input[0] != kind) {
-        throw new DecodeError(`Expected kind ${kind}, got ${input[0]}`);
-    }
+  if (input[0] != kind) {
+    throw new DecodeError(`Expected kind ${kind}, got ${input[0]}`);
+  }
 
-    const argCount = input.length - 1;
-    if (argCount != argDecoders.length) {
-        throw new DecodeError(`Expected ${argDecoders.length} args for custom type "${kind}", got ${argCount}`);
-    }
+  const argCount = input.length - 1;
+  if (argCount != argDecoders.length) {
+    throw new DecodeError(
+      `Expected ${argDecoders.length} args for custom type "${kind}", got ${argCount}`
+    );
+  }
 
-    var result = {
-        kind: kind
-    }
+  var result = {
+    kind: kind,
+  };
 
-    for (var i = 0; i < argDecoders.length; i++) {
-        var paramName = argNames[i];
-        result[paramName] = argDecoders[i](input[i + 1]);
-    }
+  for (var i = 0; i < argDecoders.length; i++) {
+    var paramName = argNames[i];
+    result[paramName] = argDecoders[i](input[i + 1]);
+  }
 
-    return result;
+  return result;
 }
 
-export function decodeDict<K,V>(decodeKey: (any) => K, decodeValue: (any) => V, input: any): Map<K,V> {
-    if (!(input instanceof Array)) {
-        throw new DecodeError(`Expected array, got ${typeof(input)}`);
+export function decodeDict<K, V>(
+  decodeKey: (any) => K,
+  decodeValue: (any) => V,
+  input: any
+): Map<K, V> {
+  if (!(input instanceof Array)) {
+    throw new DecodeError(`Expected array, got ${typeof input}`);
+  }
+
+  const inputArray: Array<any> = input;
+
+  return new Map(inputArray.map((item: any) => {
+    if (!(item instanceof Array)) {
+      throw new DecodeError(`Expected array, got ${typeof item}`);
     }
 
-    const inputArray: Array<any> = input;
-
-    return new Map(inputArray.map((item: any) => {
-        if (!(item instanceof Array)) {
-            throw new DecodeError(`Expected array, got ${typeof(item)}`);
-        }
-
-        const itemArray: Array<any> = item;
-        return [decodeKey(itemArray[0]), decodeValue(itemArray[1])];
-    }));
+    const itemArray: Array<any> = item;
+    return [decodeKey(itemArray[0]), decodeValue(itemArray[1])];
+  }));
 }
 
-export function decodeList<T>(decodeElement: (any) => T,
-                              input: any): Array<T> {
-    if (!(input instanceof Array)) {
-        throw new DecodeError(`Expected Array, got ${typeof(input)}`);
-    }
+export function decodeList<T>(decodeElement: (any) => T, input: any): Array<T> {
+  if (!(input instanceof Array)) {
+    throw new DecodeError(`Expected Array, got ${typeof input}`);
+  }
 
-    const inputArray: Array<any> = input;
-    return inputArray.map(decodeElement);
+  const inputArray: Array<any> = input;
+  return inputArray.map(decodeElement);
 }
 
 export function decodeRecord(fieldDecoders: DecoderMap, input: any): object {
-    if (!(input instanceof Object)) {
-        throw new DecodeError(`Expected Object, got ${typeof(input)}`);
+  if (!(input instanceof Object)) {
+    throw new DecodeError(`Expected Object, got ${typeof input}`);
+  }
+
+  const inputObject: object = input;
+
+  const fieldNames: Array<string> = Array.from(fieldDecoders.keys());
+  for (var field in fieldNames) {
+    if (!(field in Object.keys(input))) {
+      throw new DecodeError(`Expected field ${field} was not found`);
     }
+  }
+  if (Object.keys(inputObject).length > fieldNames.length) {
+    throw new DecodeError(
+      `Input object has extra fields, expected ${fieldNames.length}, got ${
+        input.keys().length
+      }`
+    );
+  }
 
-    const inputObject: object = input;
-
-    const fieldNames: Array<string> = Array.from(fieldDecoders.keys());
-    for (var field in fieldNames) {
-        if (!(field in Object.keys(input))) {
-            throw new DecodeError(`Expected field ${field} was not found`);
-        }
+  var result = new Object();
+  fieldDecoders.forEach((decoder: GenericDecoder, name: string) => {
+    if (!(name in inputObject)) {
+      throw new DecodeError(`Input record object missing field: ${name}`);
     }
-    if (Object.keys(inputObject).length > fieldNames.length) {
-        throw new DecodeError(`Input object has extra fields, expected ${fieldNames.length}, got ${input.keys().length}`);
-    }
+    result[name] = decoder(inputObject[name]);
+  });
 
-    var result = new Object;
-    fieldDecoders.forEach((decoder: GenericDecoder, name: string) => {
-        if (!(name in inputObject)) {
-            throw new DecodeError(`Input record object missing field: ${name}`);
-        }
-        result[name] = decoder(inputObject[name]);
-    });
-
-    return result;
+  return result;
 }
 
-export function decodeTuple(elementDecoders: DecoderList, input: any): Array<any> {
-    if (!(input instanceof Array)) {
-        throw new DecodeError(`Expected Array, got ${typeof(input)}`);
-    }
+export function decodeTuple(
+  elementDecoders: DecoderList,
+  input: any
+): Array<any> {
+  if (!(input instanceof Array)) {
+    throw new DecodeError(`Expected Array, got ${typeof input}`);
+  }
 
-    const inputArray: Array<any> = input;
-    let result = [];
-    for (var i = 0; i < inputArray.length; i++) {
-        result.push(elementDecoders[i](inputArray[i]));
-    }
-    return result;
+  const inputArray: Array<any> = input;
+  let result = [];
+  for (var i = 0; i < inputArray.length; i++) {
+    result.push(elementDecoders[i](inputArray[i]));
+  }
+  return result;
 }
 
 export function encodeUnit(value: []): any {
-    return {};
+  return {};
 }
 
 export function encodeBoolean(value: boolean): boolean {
-    return value;
+  return value;
 }
 
 export function encodeChar(value: string): string {
-    return value;
+  return value;
 }
 
 export function encodeString(value: string): string {
-    return value;
+  return value;
 }
 
 export function encodeInt(value: number): number {
-    return value;
+  return value;
 }
 
 export function encodeFloat(value: number): number {
-    return value;
+  return value;
 }
 
 export function encodeCustomType(encoderMap: EncoderMap, value: any): any {
-    if (encoderMap.has(value["kind"])) {
-        const encoderFn: any = encoderMap.get(value["kind"]);
-        return encoderFn(value);
-    } else {
-        throw new DecodeError(`Didn't find encoder for type variant: ${value["kind"]}`);
+  if (encoderMap.has(value["kind"])) {
+    const encoderFn: any = encoderMap.get(value["kind"]);
+    return encoderFn(value);
+  } else {
+    throw new DecodeError(
+      `Didn't find encoder for type variant: ${value["kind"]}`
+    );
+  }
+}
+
+export function encodeCustomTypeVariant(
+  argNames: Array<string>,
+  argEncoders: EncoderList,
+  value: object
+): Array<any> {
+  if (argNames.length == 0) {
+    return value["kind"];
+  } else {
+    var result = [value["kind"]];
+    for (var i = 0; i < argNames.length; i++) {
+      const name = argNames[i];
+      result.push(argEncoders[i](value[name]));
     }
-}
-
-export function encodeCustomTypeVariant(argNames: Array<string>,
-                                        argEncoders: EncoderList,
-                                        value: object,): Array<any> {
-    if (argNames.length == 0) {
-        return value['kind'];
-    } else {
-        var result = [value['kind']];
-        for (var i = 0; i < argNames.length; i++) {
-            const name = argNames[i];
-            result.push(argEncoders[i](value[name]));
-        };
-        return result;
-    }
-}
-
-export function encodeDict<K,V>(encodeKey: (any) => K,
-                                encodeValue: (any) => V,
-                                value: Map<K,V>): Array<[K,V]> {
-    return Array.from(value.entries()).map((pair: [K,V]): [K,V] => {
-        return [encodeKey(pair[0]), encodeValue(pair[1])];
-    });
-}
-
-export function encodeList<T>(encodeElement: (any) => T,
-                              value: Array<T>) {
-    return value.map(encodeElement);
-}
-
-export function encodeRecord(fieldEncoders: EncoderMap,
-                             value: object): object {
-    let result = new Object;
-    fieldEncoders.forEach((encoder: GenericEncoder, name: string) => {
-        result[name] = encoder(value[name]);
-    });
     return result;
+  }
 }
 
-export function encodeTuple(elementEncoders: EncoderList,
-                            value: Array<any>): Array<any> {
-   let result = new Array;
-    for (var i = 0; i < value.length; i++) {
-        result.push(elementEncoders[i](value[i]));
-    };
-    return result;
+export function encodeDict<K, V>(
+  encodeKey: (any) => K,
+  encodeValue: (any) => V,
+  value: Map<K, V>
+): Array<[K, V]> {
+  return Array.from(value.entries()).map((pair: [K, V]): [K, V] => {
+    return [encodeKey(pair[0]), encodeValue(pair[1])];
+  });
+}
+
+export function encodeList<T>(encodeElement: (any) => T, value: Array<T>) {
+  return value.map(encodeElement);
+}
+
+export function encodeRecord(fieldEncoders: EncoderMap, value: object): object {
+  let result = new Object();
+  fieldEncoders.forEach((encoder: GenericEncoder, name: string) => {
+    result[name] = encoder(value[name]);
+  });
+  return result;
+}
+
+export function encodeTuple(
+  elementEncoders: EncoderList,
+  value: Array<any>
+): Array<any> {
+  let result = new Array();
+  for (var i = 0; i < value.length; i++) {
+    result.push(elementEncoders[i](value[i]));
+  }
+  return result;
 }
